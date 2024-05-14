8fc63974d8335d2f6fbdde2f543489dfce12ea4e
kxh4892636 <kxh4892636@gmail.com>
2024-05-14 20:29:15 +0800

    fix: diff model

--- util/model/multi/powerIndex.py
+++ util/model/multi/powerIndex.py
@@ -11,76 +11,73 @@ from multiUtil import (
     getVelOfDeepestPointOnSection,
 )
 
-
-def main(jsonPath: str, date: str, condition: str):
-    content: dict = {}
-    # read json
-    with open(jsonPath, "r", encoding="utf8") as file:
-        content = json.load(file)
-    scene: str = content.get("scene")  # type: ignore
-    dataFolderPath: str = content.get("dataFolderPath")  # type: ignore
-    # vertex: tuple[tuple[float, float], tuple[float, float]] = [  # type: ignore
-    #     tuple(point)
-    #     for point in content.get("vertex")  # type: ignore
-    # ]
-
-    # path
-    depthCsvPath = os.path.join(dataFolderPath, "csv", f"{scene}-depth.csv")
-    indexPath = os.path.join(dataFolderPath, "csv", f"{scene}-index.pkl")
-    levelPath = os.path.join(
-        dataFolderPath, "csv", f"{scene}-level-{condition}.csv"
-    )
-    velPath = os.path.join(
-        dataFolderPath, "csv", f"{scene}-vel-{condition}.csv"
-    )
-    year = date.split("-")[0]
-    # currentDemPath = os.path.join(
-    #     dataFolderPath, "raster", f"{date.replace('-','')}.tif"
-    # )
-
-    # section
-    currentSectionPointsList: list = content.get("section")  # type: ignore
-    currentSectionPoints: tuple = tuple(
-        tuple(point) for point in currentSectionPointsList
-    )
-
-    # params
-    currentPointCoord = [(point[0], point[1]) for point in currentSectionPoints]
-    deepestPointInfo = getDeepestPointInfoOnSection(
-        currentPointCoord, depthCsvPath, indexPath
-    )
-    vel = getVelOfDeepestPointOnSection(
-        deepestPointInfo[0],
-        velPath,
-    )
-    level = getLevelOfDeepestPointOnSection(
-        deepestPointInfo[0],
-        levelPath,
-    )
-
-    # pq
-    pqList = [0.8 for i in range(0, 34)]
-    PQIndex = computePQIndex(int(year), pqList)
-    # ky
-    KYIndex = computeKYIndex(vel)
-    # Zd
-    ZDIndex = computeZdIndex(level, deepestPointInfo[1], condition)
-
-    # write json
-    content["PQ"] = PQIndex
-    content["KY"] = KYIndex
-    content["ZD"] = ZDIndex
-    with open(jsonPath, "w", encoding="utf8") as f:
-        json.dump(content, f, ensure_ascii=False)
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("参数错误！")
     else:
         # json 路径, 水文年, 水文状况
-        [jsonPath, date, condition] = sys.argv[1:4]
+        [jsonPath, year, condition] = sys.argv[1:4]
+
+        content: dict = {}
+        # read json
+        with open(jsonPath, "r", encoding="utf8") as file:
+            content = json.load(file)
+        scene: str = content.get("scene")  # type: ignore
+        dataFolderPath: str = content.get("dataFolderPath")  # type: ignore
+        vertex: tuple[tuple[float, float], tuple[float, float]] = [  # type: ignore
+            tuple(point)
+            for point in content.get("vertex")  # type: ignore
+        ]
+
+        # path
+        depthCsvPath = os.path.join(dataFolderPath, "csv", f"{scene}-depth.csv")
+        indexPath = os.path.join(dataFolderPath, "csv", f"{scene}-index.pkl")
+        levelPath = os.path.join(
+            dataFolderPath, "csv", f"{scene}-level-{condition}.csv"
+        )
+        velPath = os.path.join(
+            dataFolderPath, "csv", f"{scene}-vel-{condition}.csv"
+        )
+        currentDemPath = os.path.join(
+            dataFolderPath, "raster", f"{year}Before.tif"
+        )
+
+        # section
+        currentSectionPointsList: list = content.get("section")  # type: ignore
+        currentSectionPoints: tuple = tuple(
+            tuple(point) for point in currentSectionPointsList
+        )
+
+        # params
+        currentPointCoord = [
+            (point[0], point[1]) for point in currentSectionPoints
+        ]
+        deepestPointInfo = getDeepestPointInfoOnSection(
+            currentPointCoord, depthCsvPath, indexPath
+        )
+        vel = getVelOfDeepestPointOnSection(
+            deepestPointInfo[0],
+            velPath,
+        )
+        level = getLevelOfDeepestPointOnSection(
+            deepestPointInfo[0],
+            levelPath,
+        )
+
+        # pq
+        pqList = [0.8 for i in range(0, 34)]
+        PQIndex = computePQIndex(int(year), pqList)
+        # ky
+        KYIndex = computeKYIndex(vel)
+        # Zd
+        ZDIndex = computeZdIndex(level, deepestPointInfo[1], condition)
+
+        # write json
+        content["PQ"] = PQIndex
+        content["KY"] = KYIndex
+        content["ZD"] = ZDIndex
+        with open(jsonPath, "w", encoding="utf8") as f:
+            json.dump(content, f, ensure_ascii=False)
 
         # test
-        # D:\project\JS_Bank_Collapse\util\model\multi\test\result.json 2022-03-01 dry
-        main(jsonPath, date, condition)
+        # D:\project\JS_Bank_Collapse\util\model\multi\test\result.json 2022 dry
--- util/model/multi/section.py
+++ util/model/multi/section.py
@@ -2,57 +2,11 @@ import json
 import os
 import sys
 
-from multiUtil import getSectionPointList, isSectionInArea
+from multiUtil import getSectionPointList
 from osgeo import gdal
 
-
-def main(
-    x1: str,
-    y1: str,
-    x2: str,
-    y2: str,
-    date: str,
-    scene: str,
-    dataFolderPath: str,
-    dstPath: str,
-):
-    # section
-    currentDemPath = os.path.join(
-        dataFolderPath, "raster", f"{scene}-{date.replace('-','')}.tif"
-    )
-    currentDataset: gdal.Dataset = gdal.Open(currentDemPath)
-    currentSectionPoints = getSectionPointList(
-        currentDataset, float(x1), float(y1), float(x2), float(y2)
-    )
-    isAllInArea = isSectionInArea(
-        currentDataset, float(x1), float(y1), float(x2), float(y2)
-    )
-
-    vertex0 = None
-    vertex1 = None
-    if currentSectionPoints[0][2] > currentSectionPoints[-1][2]:
-        vertex0 = (x1, y1)
-        vertex1 = (x2, y2)
-    else:
-        vertex0 = (x2, y2)
-        vertex1 = (x1, y1)
-        currentSectionPoints.reverse()
-
-    # write json
-    content = {
-        "date": date,
-        "vertex": (vertex0, vertex1),
-        "scene": scene,
-        "dataFolderPath": dataFolderPath,
-        "section": currentSectionPoints,
-        "isSectionWithInArea": [isAllInArea, True],
-    }
-    with open(dstPath, "w", encoding="utf8") as f:
-        json.dump(content, f)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 9:
+    if len(sys.argv) != 10:
         print("参数错误！")
     else:
         [
@@ -60,16 +14,33 @@ if __name__ == "__main__":
             y1,
             x2,  # 终点
             y2,
-            date,  # 年份
+            year,  # 年份
+            tag,  # before or after
             scene,  # 场景, 目前恒为 sanshawan
             dataFolderPath,  # 数据文件夹
             dstPath,  # 输出 json 路径
-        ] = sys.argv[1:9]
+        ] = sys.argv[1:10]
+
+        tag = tag.capitalize()
+        # section
+        currentDemPath = os.path.join(
+            dataFolderPath, "raster", f"{year}{tag}.tif"
+        )
+        currentDataset: gdal.Dataset = gdal.Open(currentDemPath)
+        currentSectionPoints = getSectionPointList(
+            currentDataset, float(x1), float(y1), float(x2), float(y2)
+        )
+
+        # write json
+        content = {
+            "year": year,
+            "vertex": ((x1, y1), (x2, y2)),
+            "scene": scene,
+            "dataFolderPath": dataFolderPath,
+            "section": currentSectionPoints,
+        }
+        with open(dstPath, "w", encoding="utf8") as f:
+            json.dump(content, f)
 
         # test
-        # 13417691 3767754 13417931 3767270 20230901 minzhusha D:\project\JS_Bank_Collapse\util\model\multi\data D:\project\JS_Bank_Collapse\util\model\multi\test\result.json
-        # con
-        # 13417931 3767270 13417691 3767754 20230901 minzhusha D:\project\JS_Bank_Collapse\util\model\multi\data D:\project\JS_Bank_Collapse\util\model\multi\test\result.json
-        # -999
-        # 13414573 3769008 13414748 3768302 20230901 minzhusha D:\project\JS_Bank_Collapse\util\model\multi\data D:\project\JS_Bank_Collapse\util\model\multi\test\result.json
-        main(x1, y1, x2, y2, date, scene, dataFolderPath, dstPath)
+        # 13415473 3768179 13415962 3767887 2023 after minzhusha D:\project\JS_Bank_Collapse\util\model\multi\data D:\project\JS_Bank_Collapse\util\model\multi\test\result.json
