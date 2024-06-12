581cd2bf97ffe498c1f5e913791af3e3d0525166
JohnnnyTang <1275441282@qq.com>
2024-06-12 15:57:30 +0800

    back to normal warning job time

--- backEnd/src/main/java/com/johnny/bank/jobs/QuartzSchedulerManager.java
+++ backEnd/src/main/java/com/johnny/bank/jobs/QuartzSchedulerManager.java
@@ -118,7 +118,7 @@ public class QuartzSchedulerManager {
                 .withIdentity("gnssWarning", "group1")
                 .build();
         // 基于表达式构建触发器
-        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0 0/59 * * * ?");
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0 0/20 * * * ?");
         // CronTrigger表达式触发器 继承于Trigger。TriggerBuilder 用于构建触发器实例
         CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("gnssWarningTrigger", "tesGroup")
                 .withSchedule(cronScheduleBuilder).build();
