4efba507d33383d9d9f392563b96960dba5b1274
1CHRY1 <119379635+1CHRY1@users.noreply.github.com>
2024-07-03 14:58:05 +0800

    20240703

--- backEnd/src/main/java/com/johnny/bank/model/configuration/JWTInterceptor.java
+++ backEnd/src/main/java/com/johnny/bank/model/configuration/JWTInterceptor.java
@@ -1,5 +1,6 @@
 package com.johnny.bank.model.configuration;
 
+import com.alibaba.fastjson2.JSON;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -7,6 +8,7 @@ import com.auth0.jwt.exceptions.AlgorithmMismatchException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.johnny.bank.utils.TokenUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -26,9 +28,14 @@ public class JWTInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Map<String,Object> map = new HashMap<>();
         String token = request.getHeader("token");
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("nanshidashuikeyuan12138")).build();
+        if (token == null || token.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            map.put("error", "Token不能为空");
+            response.getWriter().write(JSON.toJSONString(map)); // 假设使用了某个JSON库来转换Map为JSON字符串
+            return false;
+        }
         try {
-            jwtVerifier.verify(token);
+            TokenUtil.verifyToken(token);
             return true;
         } catch (SignatureVerificationException e) {
             e.printStackTrace();
