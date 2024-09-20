a3a10369a674b7eabc3bdccb4474868190ba850d
1CHRY1 <119379635+1CHRY1@users.noreply.github.com>
2024-09-20 15:20:17 +0800

    flow tide api

--- front-v2/src/main.js
+++ front-v2/src/main.js
@@ -11,53 +11,115 @@ import DataVVue3 from '@kjgl77/datav-vue3'
 import { DraggablePlugin } from '@braks/revue-draggable'
 import { useStatusStore } from './store/statusStore'
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
 
-const login = import.meta.env.VITE_LOGIN
-if (login === 'NOT') {
-    // nothing
-} else if (login === 'YES') {
-    // interceptors config
-    axios.interceptors.request.use(
-        config => {
-            const token = localStorage.getItem('token')
-            if (token) {
-                config.headers["token"] = token
+// const requestIp = new Promise((res) => {
+//     setTimeout(() => {
+//         res('hello')
+//     }, 5000);
+// }).then((res) => {
+    //   
+    const pinia = createPinia()
+    pinia.use(piniaPluginPersistedstate)
+
+    const login = import.meta.env.VITE_LOGIN
+    if (login === 'NOT') {
+        // nothing
+    } else if (login === 'YES') {
+        // interceptors config
+        axios.interceptors.request.use(
+            config => {
+                const token = localStorage.getItem('token')
+                if (token) {
+                    config.headers["token"] = token
+                }
+                return config
+            },
+            error => {
+                return Promise.reject(error);
             }
-            return config
-        },
-        error => {
-            return Promise.reject(error);
-        }
-    )
-    axios.interceptors.response.use(
-        response => {
-            if (response.data["state"] === false) {
-                ElMessage.error("用户认证未通过，请重新登录")
-                router.push('/login')
-                useStatusStore().loginStatus = false
-                return Promise.reject(response.data)
+        )
+        axios.interceptors.response.use(
+            response => {
+                if (response.data["state"] === false) {
+                    ElMessage.error("用户认证未通过，请重新登录")
+                    router.push('/login')
+                    useStatusStore().loginStatus = false
+                    return Promise.reject(response.data)
+                }
+                return response
+            },
+            error => {
+                return Promise.reject(error)
             }
-            return response
-        },
-        error => {
-            return Promise.reject(error)
-        }
-    )
-}
+        )
+    }
 
 
 
-const app = createApp(App)
-    .use(DraggablePlugin)
-    .use(router)
-    .use(DataVVue3)
-    .use(ElementPlus)
-    .use(pinia)
+    const app = createApp(App)
+        .use(DraggablePlugin)
+        .use(router)
+        .use(DataVVue3)
+        .use(ElementPlus)
+        .use(pinia)
 
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component)
+    }
 
-app.mount('#app')
+    app.mount('#app')
+
+
+// })
+
+
+// const pinia = createPinia()
+// pinia.use(piniaPluginPersistedstate)
+
+// const login = import.meta.env.VITE_LOGIN
+// if (login === 'NOT') {
+//     // nothing
+// } else if (login === 'YES') {
+//     // interceptors config
+//     axios.interceptors.request.use(
+//         config => {
+//             const token = localStorage.getItem('token')
+//             if (token) {
+//                 config.headers["token"] = token
+//             }
+//             return config
+//         },
+//         error => {
+//             return Promise.reject(error);
+//         }
+//     )
+//     axios.interceptors.response.use(
+//         response => {
+//             if (response.data["state"] === false) {
+//                 ElMessage.error("用户认证未通过，请重新登录")
+//                 router.push('/login')
+//                 useStatusStore().loginStatus = false
+//                 return Promise.reject(response.data)
+//             }
+//             return response
+//         },
+//         error => {
+//             return Promise.reject(error)
+//         }
+//     )
+// }
+
+
+
+// const app = createApp(App)
+//     .use(DraggablePlugin)
+//     .use(router)
+//     .use(DataVVue3)
+//     .use(ElementPlus)
+//     .use(pinia)
+
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
+
+// app.mount('#app')
