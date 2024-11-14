ca66e8ccd7cf3b4d35913c96cb0674bc50d42c19
Stevnda <1849698643@qq.com>
2024-11-14 21:33:23 +0800

    update segmentMange in both frontEnd and backEnd

---
+++ backEnd-v2/src/main/java/com/johnny/bank/repository/resourceRepo/dataResourceRepo/ImportantBankRepo.java
@@ -0,0 +1,7 @@
+package com.johnny.bank.repository.resourceRepo.dataResourceRepo;/**
+* @author: DMK
+* @description:  
+* @date: 2024-11-08 21:23:10
+* @version: 1.0
+*/public interface ImportantBankRepo {
+}
---
+++ backEnd-v2/src/main/java/com/johnny/bank/repository/resourceRepo/dataResourceRepo/SectionDataRepo.java
@@ -0,0 +1,7 @@
+package com.johnny.bank.repository.resourceRepo.dataResourceRepo;/**
+* @author: DMK
+* @description:  
+* @date: 2024-11-08 15:41:37
+* @version: 1.0
+*/public interface SectionDataRepo {
+}
